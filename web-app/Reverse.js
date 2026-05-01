2a5df2ad2e2a878c7218136b27c354cc833ae228
fourier-space <freddie.page@imperial.ac.uk>
2026-05-01 13:46:18 +0100

    2025-26

--- web-app/Reverse.js
+++ web-app/Reverse.js
@@ -14,7 +14,7 @@ const Reverse = Object.create(null);
  * @returns {string} The same string with the order of it's characters reversed.
  */
 Reverse.reverse = function (string) {
-    // Implemention is to convert the string to an array of characters.
+    // Implementation is to convert the string to an array of characters.
     // Reverse the order of the array, then recombining into a single string.
     return string.split("").reverse().join("");
 };
